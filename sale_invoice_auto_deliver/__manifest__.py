870acd49671421f383afd3e6b65ad76356ba852a
xavier-bouquiaux <xavier.bouquiaux@acsone.eu>
2021-05-06 21:13:12 +0200

    [14.0][MIG] sale_invoice_auto_deliver

--- sale_invoice_auto_deliver/__manifest__.py
+++ sale_invoice_auto_deliver/__manifest__.py
@@ -1,9 +1,9 @@
-# © 2016  Laetitia Gangloff, Acsone SA/NV (http://www.acsone.eu)
+# © 2021  Laetitia Gangloff, Acsone SA/NV (http://www.acsone.eu)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Sale Invoice Automatic Deliver",
-    "version": "10.0.1.0.0",
+    "version": "14.0.1.0.0",
     "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
     "category": "Sales Management",
     "website": "https://github.com/OCA/sale-workflow",
