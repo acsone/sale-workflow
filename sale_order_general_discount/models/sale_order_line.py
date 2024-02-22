6d40a60b2dfc30ba95a3fe33f11a950e71bdfe57
Justine Doutreloux <justine.doutreloux@acsone.eu>
2024-02-22 17:51:50 +0100

    [FIX] sale_order_general_discount: fix compute_discount: Set general discount as line discount only if there's a general discount.

--- sale_order_general_discount/models/sale_order_line.py
+++ sale_order_general_discount/models/sale_order_line.py
@@ -1,16 +1,10 @@
 # Copyright 2018 Tecnativa - Sergio Teruel
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-from odoo import api, fields, models
+from odoo import api, models
 
 
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
-
-    discount = fields.Float(
-        compute="_compute_discount",
-        store=True,
-        readonly=False,
-    )
 
     @api.model_create_multi
     def create(self, vals_list):
@@ -26,8 +20,8 @@ class SaleOrderLine(models.Model):
 
     @api.depends("order_id", "order_id.general_discount")
     def _compute_discount(self):
-        if hasattr(super(), "_compute_discount"):
-            super()._compute_discount()
+        res = super()._compute_discount()
         for line in self:
-            line.discount = line.order_id.general_discount
-        return
+            if line.order_id.general_discount:
+                line.discount = line.order_id.general_discount
+        return res
