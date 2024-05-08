1c5372f534dab7e131108f2acf4a0ffabe6d5601
Jacques-Etienne Baudoux <je@bcim.be>
2024-05-08 13:13:28 +0200

    [IMP] sale_order_line_cancel: support kits

--- sale_order_line_cancel/models/sale_order.py
+++ sale_order_line_cancel/models/sale_order.py
@@ -1,11 +1,11 @@
 # Copyright 2023 ACSONE SA/NV
+# Copyright 2024 Jacques-Etienne Baudoux (BCIM) <je@bcim.be>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import models
 
 
 class SaleOrder(models.Model):
-
     _inherit = "sale.order"
 
     def action_draft(self):
@@ -16,14 +16,18 @@ class SaleOrder(models.Model):
 
     def _action_cancel(self):
         orders = self.filtered(lambda s: s.state != "cancel")
-        orders_with_picking = orders.filtered("picking_ids")
-        res = None
-        if orders_with_picking:
-            orders_with_picking = orders_with_picking.with_context(
-                orders_cancel_by_running_procurements=orders_with_picking.ids
+        orders_with_stock_move = orders.filtered(
+            lambda o: any(
+                line.qty_delivered_method == "stock_move" for line in o.order_line
             )
-            res = super(SaleOrder, orders_with_picking)._action_cancel()
-        remaining_orders = orders - orders_with_picking
+        )
+        res = None
+        if orders_with_stock_move:
+            orders_with_stock_move = orders_with_stock_move.with_context(
+                orders_cancel_by_running_procurements=orders_with_stock_move.ids
+            )
+            res = super(SaleOrder, orders_with_stock_move)._action_cancel()
+        remaining_orders = orders - orders_with_stock_move
         if remaining_orders:
             res = super(SaleOrder, remaining_orders)._action_cancel()
 
--- sale_order_line_cancel/models/stock_move.py
+++ sale_order_line_cancel/models/stock_move.py
@@ -1,11 +1,11 @@
 # Copyright 2023 ACSONE SA/NV
+# Copyright 2024 Jacques-Etienne Baudoux (BCIM) <je@bcim.be>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import models
 
 
 class StockMove(models.Model):
-
     _inherit = "stock.move"
 
     def _action_cancel(self):
@@ -13,8 +13,10 @@ class StockMove(models.Model):
             lambda m: m.sale_line_id and m.state not in ("done", "cancel")
         )
         res = super()._action_cancel()
-        for rec in sale_moves:
-            if rec.state != "cancel":
+        sale_lines = sale_moves.filtered(lambda m: m.state == "cancel").sale_line_id
+        for line in sale_lines:
+            # Update SO line qty canceled only when all remaining moves are canceled
+            if line._get_moves_to_cancel():
                 continue
-            rec.sale_line_id.product_qty_canceled = rec.product_uom_qty
+            line.product_qty_canceled = line.qty_to_deliver
         return res
