031aab3411960dd48d4d8205e5906702a752418b
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2024-12-19 15:30:46 +0100

    [ADD] sale_stock_prebook_cancel_line: Glue addon This addon is a technical module that ensures that the computation of the cancelled quantity on a sale order line is correct even when a prebooking is involved. Cancelled prebooked quantities must not be taken into account in the computation of the cancelled quantity on the sale order line.

---
+++ sale_stock_prebook_cancel_line/models/stock_move.py
@@ -0,0 +1,14 @@
+# Copyright 2024 ACSONE SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import models
+
+
+class StockMove(models.Model):
+
+    _inherit = "stock.move"
+
+    def _is_move_to_take_into_account_for_qty_canceled(self):
+        self.ensure_one()
+        res = super()._is_move_to_take_into_account_for_qty_canceled()
+        return res and not self.used_for_sale_reservation
