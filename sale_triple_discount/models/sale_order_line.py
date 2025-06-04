dfb33eb6d9231daf3db139a3847dc97bbb49e03e
sbejaoui <souheil.bejaoui@acsone.eu>
2025-06-04 15:39:06 +0200

    [IMP] sale_triple_discount: add inverse for standard discount field
    add an inverse to the standard discount field, which resets the three
    discounts and sets discount1 with the value coming from the inverse.
    this way, any standard process like pricelists will integrate with this
    logic out of the box, without extra customization

--- sale_triple_discount/models/sale_order_line.py
+++ sale_triple_discount/models/sale_order_line.py
@@ -21,21 +21,34 @@ class SaleOrderLine(models.Model):
     discount = fields.Float(
         string="Total Disc (%)",
         compute="_compute_discount",
+        inverse="_compute_discounts",  # pylint: disable=method-inverse
         store=True,
         readonly=True,
         digits=None,
     )
     discount1 = fields.Float(
+        compute="_compute_discounts",
+        precompute=True,
+        store=True,
+        readonly=False,
         string="Disc. 1 (%)",
         digits="Discount",
         default=0.0,
     )
     discount2 = fields.Float(
+        compute="_compute_discounts",
+        precompute=True,
+        store=True,
+        readonly=False,
         string="Disc. 2 (%)",
         digits="Discount",
         default=0.0,
     )
     discount3 = fields.Float(
+        compute="_compute_discounts",
+        precompute=True,
+        store=True,
+        readonly=False,
         string="Disc. 3 (%)",
         digits="Discount",
         default=0.0,
@@ -96,8 +109,15 @@ class SaleOrderLine(models.Model):
 
     @api.depends("discount1", "discount2", "discount3", "discounting_type")
     def _compute_discount(self):
+        res = super()._compute_discount()
         for rec in self:
             rec.discount = rec._get_final_discount()
+        return res
+
+    @api.depends("discount")
+    def _compute_discounts(self):
+        for rec in self:
+            rec.update({"discount1": rec.discount, "discount2": 0, "discount3": 0})
 
     _sql_constraints = [
         (
